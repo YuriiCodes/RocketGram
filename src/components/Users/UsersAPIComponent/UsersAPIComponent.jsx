647c8606e92d4c7f33530259b57d27c4869915c3
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2022-04-03 15:44:31 +0300

    added unique links for each user profile

--- src/components/Users/UsersAPIComponent/UsersAPIComponent.jsx
+++ src/components/Users/UsersAPIComponent/UsersAPIComponent.jsx
@@ -2,28 +2,7 @@ import React, {useEffect} from "react";
 import axios from "axios";
 import Users from "./UsersPresentationalComponent/Users";
 import Preloader from "../../Common/Preloader/Preloader";
-import {
-    useLocation,
-    useNavigate,
-    useParams,
-} from "react-router-dom";
 
-// wrapper to use react router's v6 hooks in class comopnent(to use HOC pattern, like in router v5)
-function withRouter(Component) {
-    function ComponentWithRouterProp(props) {
-        let location = useLocation();
-        let navigate = useNavigate();
-        let params = useParams();
-        return (
-            <Component
-                {...props}
-                router={{ location, navigate, params }}
-            />
-        );
-    }
-
-    return ComponentWithRouterProp;
-}
 
 class UsersAPIComponent extends React.Component {
     componentDidMount() {
