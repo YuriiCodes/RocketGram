e77af78ff4d3d4ab11b89e3981ffc54123fe8977
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2022-04-04 12:39:06 +0300

    Refactored api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
+
+// Agxios instance for api calls
+const instance = axios.create({
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    withCredentials: true,
+    headers: {
+        "API-KEY": "f796bcd4-c5a2-4f86-88ad-961815cc5b76"
+    }
+})
+
 const usersAPI = {
     getUsers(pageNumber, usersPerPage) {
-        return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${usersPerPage}`, {withCredentials: true})
+        return instance.get(`users?page=${pageNumber}&count=${usersPerPage}`)
             .then(res => res.data)
     },
     unfollow(id) {
-        return axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {
-            withCredentials: true,
-            headers: {
-                "API-KEY": "f796bcd4-c5a2-4f86-88ad-961815cc5b76"
-            }
-        }).then(res => res.data)
+        return instance.delete(`follow/${id}`).then(res => res.data)
     },
    follow(id) {
-      return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {}, {
-           withCredentials: true,
-           headers: {
-               "API-KEY": "f796bcd4-c5a2-4f86-88ad-961815cc5b76"
-           }
-       }).then(res => res.data)
+      return instance.post(`follow/${id}`).then(res => res.data)
    }
 }
 
