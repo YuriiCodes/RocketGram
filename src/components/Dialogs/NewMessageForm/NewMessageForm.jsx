1c8a2e02c43cbcc96e1be0f9d7448ff51a6e122c
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2022-04-27 22:55:40 +0300

    Added validation for all forms

--- src/components/Dialogs/NewMessageForm/NewMessageForm.jsx
+++ src/components/Dialogs/NewMessageForm/NewMessageForm.jsx
@@ -1,6 +1,6 @@
 import classes from "./NewMessageForm.module.css"
-import {messageChangeActionCreator, sendMessageActionCreator} from "../../../data/dialogsReducer";
-
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
 
 function NewMessageForm(props) {
 
@@ -13,13 +13,43 @@ function NewMessageForm(props) {
         props.updateMessage(e.target.value);
 
     }
+
+    const formik = useFormik({
+        initialValues: {
+            message: props.newMessageText
+        },
+        validationSchema: Yup.object({
+            message: Yup.string()
+                .required('Required')
+                .min(2, 'Too Short!')
+                .max(70, 'Too Long!')
+        }),
+        onSubmit: values => {
+            props.sendMessage();
+        }
+    });
     return (
-        <form className={classes.form} onSubmit={onSendMessage}>
-            <input  type="text" onChange={onMessageChange} value={props.newMessageText}
-                   className={classes.input}/>
-            <button onClick={onSendMessage} className={classes.submit}>
+        <form className={classes.form} onSubmit={formik.handleSubmit}>
+            <input
+                type="text"
+                value={props.newMessageText}
+                className={classes.input}
+
+                name={"message"}
+                id={"message"}
+
+                onBlur={formik.handleBlur}
+                onChange={(e) => {
+                    formik.handleChange(e);
+                    props.updateMessage(e.target.value);
+                }}
+            />
+            <button type={"submit"} className={classes.submit}>
                 Send
             </button>
+            {formik.touched.message && formik.errors.message ? (
+                <div className={classes.error}>{formik.errors.message}</div>
+            ) : null}
         </form>
     )
 }
--- src/components/Profile/PostsList/newPostForm/newPostForm.jsx
+++ src/components/Profile/PostsList/newPostForm/newPostForm.jsx
@@ -1,9 +1,30 @@
 import classes from "./newPostForm.module.css";
 import {useRef} from "react";
 
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
+
+
+
 function NewPostForm(props) {
     const newPostInput = useRef(null);
 
+    const formik = useFormik({
+        initialValues: {
+            postInput: ''
+        },
+        validationSchema: Yup.object({
+            postInput: Yup.string()
+                .max(255, 'Too long!')
+                .required('Message is required')
+        }),
+        onSubmit: values => {
+            props.addPost();
+            formik.resetForm();
+            newPostInput.current.focus();
+        }
+    });
+    window.formik = formik;
     let addPost = (e) => {
         e.preventDefault();
         props.addPost();
@@ -11,21 +32,29 @@ function NewPostForm(props) {
     let onPostChange = () => {
         let text = newPostInput.current.value;
         props.updateNewPostText(text);
-        // props.dispatch(postChangeActionCreator(newPostInput.current.value));
     };
     return (
-        <form className={classes.form} onSubmit={addPost}>
+        <form className={classes.form} onSubmit={formik.handleSubmit}>
             <label htmlFor="postInput">Add new post</label>
             <input id="postInput"
                    ref={newPostInput}
                    type="text"
-                   onChange={onPostChange}
+                   onChange={(e) => {
+                       formik.handleChange(e);
+                       onPostChange();
+                   }}
+                   onBlur={formik.handleBlur}
+
                    className={classes.input}
                    name="postInput"
-                   value={props.newPostText}/>
-            <button onClick={addPost} className={classes.submit}>
+                   value={props.newPostText}
+
+            />
+            <button type={"submit"} className={classes.submit}>
                 Send
             </button>
+            {formik.touched.postInput && formik.errors.postInput ? <div className={classes.error}>{formik.errors.postInput}</div> : null}
+
         </form>
     )
 }
